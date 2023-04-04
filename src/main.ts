39cb9539f1a09047fa86793715bd515e7e18f21a
xiexuanzheng <xiexuanzheng@changba.com>
2023-04-04 18:27:35 +0800

    feat: 生成环境mock

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import 'vant/lib/index.css';
+import { setupProdMockServer } from './mock';
+if (process.env.NODE_ENV === 'production') {
+  setupProdMockServer();
+}
 
 createApp(App).mount('#app')
